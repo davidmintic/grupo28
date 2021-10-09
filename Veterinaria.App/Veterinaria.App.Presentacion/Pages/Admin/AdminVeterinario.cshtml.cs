9b65edef6afbfc164f0c8fa1c2174dbad7e2a884
davidmintic <davidperez.tic@ucaldas.edu.co>
2021-10-09 13:08:55 -0500

    Clase 09/10/2021 parte  2

--- Veterinaria.App/Veterinaria.App.Presentacion/Pages/Admin/AdminVeterinario.cshtml.cs
+++ Veterinaria.App/Veterinaria.App.Presentacion/Pages/Admin/AdminVeterinario.cshtml.cs
@@ -15,16 +15,48 @@ namespace Veterinaria.App.Presentacion.Pages
 
        private static IRepositorioVeterinario repositorioVeterinario = new RepositorioVeterinario(new Persistencia.AppContext());
 
-        public String titulo { get; set; }
+        public String titulo { get; set; } = "Diligencia los datos del administrador";
         // public List<Veterinario> listaVeterinarios = new List<Veterinario>();
         public IEnumerable<Veterinario> listaVeterinarios;
 
-        public void OnGet()
+        public Veterinario veterinarioActual;
+
+        public  String modoAdmin = "adicion";
+
+        public void OnGet(int idVeterinario)
         {
-           titulo = "Diligencia los datos del administrador";  
-           this.listaVeterinarios = repositorioVeterinario.GetVeterinarios();
-            
+
+          if(idVeterinario > 0) {
+              this.veterinarioActual = repositorioVeterinario.GetVeterinario(idVeterinario);
+              this.modoAdmin = "edicion";
+          } else {
+             this.modoAdmin = "adicion";
+          }
+
+          this.listaVeterinarios = repositorioVeterinario.GetVeterinarios();  
+                     
         }
+
+
+      public void OnPostAdd(Veterinario veterinarioNuevo){
+            repositorioVeterinario.AgregarVeterinario(veterinarioNuevo);
+            this.listaVeterinarios = repositorioVeterinario.GetVeterinarios();
+      }
+
+
+       public void OnPostEdi(Veterinario veterinarioNuevo){
+            repositorioVeterinario.EditarVeterinario(veterinarioNuevo);
+            Console.WriteLine("nombre" + veterinarioNuevo.Nombre);
+            this.listaVeterinarios = repositorioVeterinario.GetVeterinarios();
+      }
+
+      public void OnPostDel(int idVeterinario){
+            repositorioVeterinario.EliminarVeterinario(idVeterinario);
+            this.listaVeterinarios = repositorioVeterinario.GetVeterinarios();
+      }
+
+
+
 
     }
 
